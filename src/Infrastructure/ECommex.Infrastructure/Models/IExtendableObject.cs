5717538d95d08fd7b20b3e1e2428da67149ed3c0
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-06-21 21:15:12 +0000

    Updated IExtendableObject.cs

--- src/Infrastructure/ECommex.Infrastructure/Models/IExtendableObject.cs
+++ src/Infrastructure/ECommex.Infrastructure/Models/IExtendableObject.cs
@@ -9,6 +9,18 @@ namespace ECommex.Infrastructure.Models
 {
     public interface IExtendableObject
     {
-       string ExtensionData { get; set; }
+       /// <summary>
+        /// A JSON formatted string to extend the containing object.
+        /// JSON data can contain properties with arbitrary values (like primitives or complex objects).
+        /// Extension methods are available (<see cref="ExtendableObjectExtensions"/>) to manipulate this data.
+        /// General format:
+        /// <code>
+        /// {
+        ///   "Property1" : ...
+        ///   "Property2" : ...
+        /// }
+        /// </code>
+        /// </summary>
+        string ExtensionData { get; set; }
     }
 }
