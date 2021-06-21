3f1a361df5a551075ddb986d90d967a33bc1600b
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-06-21 14:02:08 +0000

    "feat(#194): implementando a classe Extendable Object Extension"

--- src/Infrastructure/ECommex.Infrastructure/Models/ExtendableObjectExtensions.cs
+++ src/Infrastructure/ECommex.Infrastructure/Models/ExtendableObjectExtensions.cs
@@ -11,8 +11,74 @@ namespace ECommex.Infrastructure.Models
 {
     public static class ExtendableObjectExtensions
     {
-        
+        public static T GetData<T>(this IExtendableObject extendableObject, string name, bool handleType = false)
+        {
+            CheckNotNull(extendableObject, name);
+            if (extendableObject == null)
+            {
+                throw new ArgumentNullException(nameof(extendableObject));
+            }
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            return extendableObject.GetData<T>(
+                name,
+                handleType
+                    ? new JsonSerializer { TypeNameHandling = TypeNameHandling.All }
+                    : JsonSerializer.CreateDefault()
+            );
 
+
+        }
+
+        public static T GetData<T>(this IExtendableObject extendableObject, string name, JsonSerializer jsonSerializer)
+        {
+            CheckNotNull(extendableObject, name);
+            if (extendableObject.ExtensionData == null)
+            {
+                return default(T);
+            }
+            var json = JObject.Parse(extendableObject.ExtensionData);
+
+            var prop = json[name];
+
+            if (prop == null)
+            {
+                return default(T);
+            }
+
+            if (TypeHelper.IsPrimitiveExtendedIncludingNullable(typeof(T)))
+            {
+                return prop.Value<T>();
+            }
+            else
+            {
+                return (T)prop.ToObject(typeof(T), jsonSerializer ?? JsonSerializer.CreateDefault());
+            }
+        }
+
+        public static void SetData<T>(this IExtendableObject extendableObject, string name, T value, bool handleType = false)
+        {
+            extendableObject.SetData(
+                name,
+                value,
+                handleType
+                    ? new JsonSerializer { TypeNameHandling = TypeNameHandling.All }
+                    : JsonSerializer.CreateDefault()
+            );
+        }
+
+        public static void SetData<T>(this IExtendableObject extendableObject, string name, T value, JsonSerializer jsonSerializer)
+        {
+            //line72
+        }
         
+        
+        
+        private static void CheckNotNull(IExtendableObject extendableObject, string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
