6bd167cecd1452fbe9405cde410ed5628aabfff9
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-06-21 21:08:07 +0000

    "feat(#194): implementando as Entidades de Infrastructure"

--- src/Infrastructure/ECommex.Infrastructure/Models/ValidatableObject.cs
+++ src/Infrastructure/ECommex.Infrastructure/Models/ValidatableObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace ECommex.Infrastructure.Models
 {
     public abstract class ValidatableObject
     {
+        public virtual bool IsValid()
+        {
+            return Validate().Count == 0;
+        }
+
+        public virtual IList<ValidationResult> Validate()
+        {
+            IList<ValidationResult> validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(this, new ValidationContext(this, null, null), validationResults, true);
+            return validationResults;
+        }
     }
 }
