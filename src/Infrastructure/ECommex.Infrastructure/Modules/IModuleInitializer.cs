e0f6ff022c2700eac4bf8ae7aaa68a95fa199154
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-06-23 20:53:24 +0000

    "Updating"

--- src/Infrastructure/ECommex.Infrastructure/Modules/IModuleInitializer.cs
+++ src/Infrastructure/ECommex.Infrastructure/Modules/IModuleInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
 
 
 namespace ECommex.Infrastructure.Modules
